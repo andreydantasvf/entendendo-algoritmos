1d558b58175640c46c58d26f5830e69f041b3c84
Andrey Dantas <andreydantasvf@gmail.com>
2025-09-10 07:43:33 -0300

    feat: componentize CodeImplementation

---
+++ src/components/layout/CodeImplementation.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { CodeBlock } from '@/components/layout/CodeBlock';
+
+export type CodesBlock = {
+  code: string;
+  language: string;
+  tabTitle: string;
+  tabValue: string;
+  title: string;
+  description: string;
+};
+
+interface CodeImplementationProps {
+  codes: CodesBlock[];
+  extraComponent?: React.ReactNode;
+}
+
+export function CodeImplementation({
+  codes,
+  extraComponent
+}: CodeImplementationProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Implementações</CardTitle>
+        <p className="text-muted-foreground text-sm">
+          Exemplos práticos do algoritmo em diferentes linguagens e abordagens
+        </p>
+      </CardHeader>
+      <CardContent>
+        <Tabs defaultValue={codes[0].tabValue} className="w-full">
+          <TabsList className="w-full gap-1 flex items-center justify-center">
+            {codes.map((code) => (
+              <TabsTrigger key={code.tabValue} value={code.tabValue}>
+                {code.tabTitle}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+
+          {codes.map((code) => (
+            <TabsContent
+              key={code.tabValue}
+              value={code.tabValue}
+              className="mt-4"
+            >
+              <div className="space-y-3">
+                <h4 className="font-semibold">{code.title}</h4>
+                <p className="text-sm text-muted-foreground">
+                  {code.description}
+                </p>
+                <CodeBlock code={code.code} language={code.language} />
+              </div>
+            </TabsContent>
+          ))}
+        </Tabs>
+
+        {/* Additional Notes */}
+        {!!extraComponent && (
+          <div className="mt-6 space-y-4">{extraComponent}</div>
+        )}
+      </CardContent>
+    </Card>
+  );
+}
